7e376e21df91d5d6030e210e648b8c3de71ef618
MoathKarem <146102795+MoathKarem@users.noreply.github.com>
2023-09-26 20:50:25 +0300

    Comment 3

--- app/src/main/java/com/example/bitjes/Techer.kt
+++ app/src/main/java/com/example/bitjes/Techer.kt
@@ -1,3 +1,11 @@
 package com.example.bitjes
 
-data class Techer(var Int)
+data class Techer(
+    var id: Int = 0,
+    var name: String = "Moath",
+
+    ) {
+    override fun toString(): String {
+        return super.toString()
+    }
+}
